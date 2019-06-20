3f4566156973ebf6280a0aa34856b2ede8214dee
Zcx <1287341777@qq.com>
2019-06-20 14:22:09 +0800

    Merge branch 'Dev1' into Dev2
    # Conflicts:
    #	script/audioComp.ts

--- script/audioComp.ts
+++ script/audioComp.ts
@@ -12,7 +12,7 @@ export default class audioComp extends cc.Component {
     private isPlay = false;
     onLoad () {
         //this.initVal();
-        cc.log('tttes13');
+        cc.log('tttes135');
         this.isPlay = true;
         this.playMusic('sound/pray/bg');
     }
